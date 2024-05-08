5693c6edb3b6a216b13e0c75137d9a50cb707387
aminerwx <aminerwx@gmail.com>
2024-05-08 22:59:57 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,74 @@
 #!/usr/bin/env bash
 
 repo="$HOME/.dotfiles-setup"
-install_pkglist() {
+install_pkg() {
 	PS3='Enter packages to install: '
-	options=(".pkglist_base.txt" ".pkglist_full.txt")
+	pkg_base=(
+		'git'
+		'neovim'
+		'ttf-jetbrains-mono-nerd'
+		'noto-fonts'
+		'fastfetch'
+		'alacritty'
+		'base-devel'
+		'bash-completion'
+		'firefox'
+		'lazygit'
+		'man-db'
+		'man-pages'
+		'ripgrep'
+		'rsync'
+		'sudo'
+		'tree'
+		'alacritty'
+		'base-devel'
+		'bash'
+		'bash-completion'
+		'btop'
+		'fastfetch'
+	)
+
+	pkg_full=(
+		'git'
+		'go'
+		'btop'
+		'neovim'
+		'lazygit'
+		'treesitter'
+		'keepassxc'
+		'lutris'
+		'man-db'
+		'man-pages'
+		'noto-fonts'
+		'nvidia'
+		'nvidia-settings'
+		'nvidia-utils'
+		'base-devel'
+		'bash-completion'
+		'postgresql'
+		'fastfetch'
+		'firefox'
+		'ripgrep'
+		'rsync'
+		'solaar'
+		'sudo'
+		'tree'
+		'ttf-jetbrains-mono-nerd'
+		'ufw'
+		'yt-dlp'
+	)
+
+	options=("pkglist_base.txt" "pkglist_full.txt")
 	select opt in "${options[@]}"; do
 		case $opt in
-		".pkglist_base.txt")
+		"pkglist_base.txt")
 			echo "Installing base packages"
-			sudo pacman -S --needed --noconfirm - <"$repo/.pkglist_base.txt"
+			sudo pacman -S --needed --noconfirm "${pkg_base[@]}"
 			break
 			;;
-		".pkglist_full.txt")
+		"pkglist_full.txt")
 			echo "Installing full packages"
-			sudo pacman -S --needed --noconfirm - <"$repo/.pkglist_full.txt"
+			sudo pacman -S --needed --noconfirm "${pkg_full[@]}"
 			break
 			;;
 		*) echo "invalid option $REPLY" ;;
@@ -22,12 +77,14 @@ install_pkglist() {
 }
 
 dotfiles_setup() {
-	install_pkglist
-	git clone --separate-git-dir=$HOME/.dotfiles https://github.com/aminerwx/.dotfiles.git tmpdotfile
-	rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-	rm -r tmpdotfiles
+	mkdir tmpdotfiles
+	git clone --separate-git-dir="$HOME/.dotfiles https://github.com/aminerwx/.dotfiles.git" tmpdotfiles
+	rsync --recursive --verbose --exclude '.git' "tmpdotfiles/" "$HOME/"
+	rm -rf tmpdotfiles
 }
 
 # TODO: handle .dotfiles
 echo "######## dotfiles setup ########"
-[ -d $repo ] && dotfiles_setup && rm -r $repo
+
+install_pkg
+dotfiles_setup
